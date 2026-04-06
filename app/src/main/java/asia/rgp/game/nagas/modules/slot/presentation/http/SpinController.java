9b97ea275ef1191fc1e2cdcc4589411a6596df46
antony <antony@antonys-MacBook-Pro.local>
2026-04-06 15:27:04 +0700

    update

--- app/src/main/java/asia/rgp/game/nagas/modules/slot/presentation/http/SpinController.java
+++ app/src/main/java/asia/rgp/game/nagas/modules/slot/presentation/http/SpinController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/slot")
+@RequestMapping("/v1/slot")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class SpinController {
